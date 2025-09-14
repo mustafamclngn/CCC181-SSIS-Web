1c469ae2a40d0023197b3bd8292b391b82d96e84
Mustafa Macalangan <mmmmacalangan@gmail.com>
2025-09-14 22:02:30 +0800

    setup for colleges,programs,students pages

--- app.py
+++ app.py
@@ -1,9 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello, World!"
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return "My name is Muhamamd Mustafa M. Macalangan"
+
+@app.route("/colleges")
+def colleges():
+    return render_template("colleges.html")
+
+@app.route("/programs")
+def programs():
+    return render_template("programs.html")
+
+@app.route("/students")
+def students():
+    return render_template("students.html")
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
