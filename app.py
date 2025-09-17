8d0dd620bda68db9ff0fc9f8a3d499a2a30d8eda
Mustafa Macalangan <mmmmacalangan@gmail.com>
2025-09-17 21:11:17 +0800

    added css animations and effects

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/")
-@app.route("/dasboard")
+@app.route("/dashboard")
 def dashboard():
     return render_template("index.html")
 
