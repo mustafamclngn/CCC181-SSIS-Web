d8fb1714d5654cdc4ed67a44611ca91b7f40318f
Mustafa Macalangan <mmmmacalangan@gmail.com>
2025-09-19 00:13:44 +0800

    changes to designs

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route("/")
 @app.route("/dashboard")
 def dashboard():
-    return render_template("index.html")
+    return render_template("dashboard.html")
 
 @app.route("/colleges")
 def colleges():
