3533e0ba369f8b58aa8d66989f5053e3626e3886
Mustafa Macalangan <mmmmacalangan@gmail.com>
2025-11-12 14:24:06 +0800

    applied CSRF protection to app

--- app/__init__.py
+++ app/__init__.py
@@ -2,8 +2,10 @@ import os
 import psycopg2
 import config
 
+from datetime import timedelta
 from dotenv import load_dotenv
 from flask import Flask, render_template, url_for, request, redirect, session
+from flask_wtf.csrf import CSRFProtect
 
 from app.database import get_db, close_db
 
@@ -19,6 +21,9 @@ def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = config.SECRET_KEY
     app.config["DATABASE_URL"] = config.DATABASE_URL
+    app.config["PERMANENT_SESSION_LIFETIME"] = timedelta(days=30)
+
+    csrf = CSRFProtect(app)
     
     app.register_blueprint(college_bp)
     app.register_blueprint(program_bp)
